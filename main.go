c131012c5bedd41cc773d35bef878cc064938522
Admin <2762713521@qq.com>
2024-07-26 00:49:06 +0800

    up

--- main.go
+++ main.go
@@ -39,7 +39,7 @@ func main() {
 
 	stream.RunCommand("git clone --progress https://chromium.googlesource.com/chromium/tools/depot_tools.git")
 	stream.RunCommand("git clone --progress -b chrome/m128 https://github.com/google/skia.git")
-	path()
+	//path()
 	AppendPathEnvWindows("depot_tools")
 
 	//githubWorkspace := os.Getenv("GITHUB_WORKSPACE")
