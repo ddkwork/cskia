11bfdf0bc243d3460ad9defa6dfcbe19e0cebf04
Admin <2762713521@qq.com>
2024-07-26 01:52:23 +0800

    m127

---
+++ todo.go
@@ -0,0 +1,12 @@
+package main
+
+/*
+CreateFile
+copy file
+做验证病关闭警告，成功输出日志
+简化cmd输出，移除line
+log init action工作区目录
+
+vc 问题
+C:\ProgramData\Chocolatey\bin\vswhere.exe -products * -requires Microsoft.Component.MSBuild -property installationPath -latest
+*/
